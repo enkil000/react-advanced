a139363288c5a33b49dd02e95768dced98be47e7
enkil000 <josetxu.sancha@gmail.com>
2022-11-16 12:41:30 +0100

    LazyLoad Base

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom'
-import {LazyPage1, LazyPage2, LazyPage3} from '../01-lazyload/pages'
+import { routes } from './routes'
 
 const Navigation = () => {
 	return (
-		<BrowserRouter>
-			<div className='main-layout'>
-				<nav>
-					<ul>
-						<li>
-							<NavLink to="/" className={({isActive})=> isActive ? 'nav-active': ''}>Lazy 1</NavLink>
-						</li>
-						<li>
-							<NavLink to="/lazy2" className={({isActive})=> isActive ? 'nav-active': ''}>Lazy 2</NavLink>
-						</li>
-						<li>
-							<NavLink to="/lazy3" className={({isActive})=> isActive ? 'nav-active': ''}>Lazy 3</NavLink>
-						</li>
-					</ul>
-				</nav>
-				<Routes>
-					<Route path="lazy2" element={<LazyPage2 />}/>
-					<Route path="lazy3" element={<LazyPage3 />}/>
-					<Route path="/" element={<LazyPage1 />}/>
-					<Route path="/*" element={<Navigate to="/lazy1" replace/>}/>
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<Suspense fallback={<span>Cargando</span>}>
+			<BrowserRouter>
+				<div className='main-layout'>
+					<nav>
+						<ul>
+							{routes.map(({ to, path, name }) => (
+								<li key={path}>
+									<NavLink to={to} className={({ isActive }) => isActive ? 'nav-active' : ''}>{name}</NavLink>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<Routes>
+						{routes.map(({ path, Component }) => (
+							<Route key={path} path={path} element={<Component />} />
+						))}
+						<Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</Suspense>
 	)
 }
 
